fad861b9528e793efa0be678de1c62c52b967d5d
deannanpham <deannapham1013@gmail.com>
2025-08-04 22:06:28 -0700

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import io
 import tempfile
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
